be6a5c184c8cc1060534d3a950da71a41bf2d6d6
TomoyaWashio <148823993+TomoyaWashio@users.noreply.github.com>
2024-08-09 13:05:36 +0900

    初回コミット

---
+++ wwwroot/script.js
@@ -0,0 +1,10 @@
+﻿igRegisterScript("onRowDelete", (args) => {
+    var result = window.confirm('行を削除しますか？');
+    if (result) {
+        console.log('OK がクリックされました');
+    }
+    else {
+        args.detail.cancel = true;
+        console.log('キャンセルがクリックされました');
+    }
+}, false);
